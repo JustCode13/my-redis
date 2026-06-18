fb6ae723f3bde645260b888fa0b41eb0e8de2505
codewithtea130 <tea858880@gmail.com>
2026-06-18 14:34:53 +0530

    Add get in KeyValueStore Class

--- kvstore/store.py
+++ kvstore/store.py
@@ -32,16 +32,33 @@ class KeyValueStore:
         self.kvstore[key] = entry
         if not self.ordered_dict.is_full():
             self.ordered_dict.touch(key)
+            self.ordered_dict.touch("hello")
         else:
             self.ordered_dict.oldest_key()
             self.ordered_dict.touch(key)
+            self.ordered_dict.touch("hello")
+
         
         self.file_storage.save(self.kvstore)
-        self.kvstore = self.file_storage.load()
+
+        print(self.kvstore["otp"].created_at)
+        print(self.kvstore["otp"].expired_at)
         
 
-    def get(self):
-        pass
+    def get(self,key):
+        if key in self.kvstore:
+            entry_obj = self.kvstore[key]
+            now = time.monotonic()
+
+            if not entry_obj.expired_at is None and now > entry_obj.expired_at:
+                    print(f"key: {key} has been expired.")
+                    self.kvstore.pop(key)
+                    self.ordered_dict.remove(key)
+            else:
+                self.ordered_dict.touch(key)
+                return self.kvstore[key]
+        else:
+            print(f"key {key} doesn't exists.")
 
     def delete(self):
         pass
--- main.py
+++ main.py
@@ -6,9 +6,17 @@ def main():
     kvstore = KeyValueStore()
     kvstore.set(
         "otp", 
-        {"userid":"123","name":"tea","age":20}
+        {"userid":"123","name":"tea","age":20},
+        1
         )
-
+    kvstore.set(
+        "shoppingcart", 
+        ["PC","Laptop","Bag"],
+        1
+        )
+    time.sleep(3)    
+    kvstore.get("otp")
+    
 
 
 if __name__ == "__main__":
